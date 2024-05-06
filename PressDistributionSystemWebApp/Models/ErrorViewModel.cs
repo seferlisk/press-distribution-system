18b73df8e84cc1b0918dcc780f44802597fb62cd
seferlisk <seferlisk@gmail.com>
2024-05-06 14:35:52 +0300

    vanilla asp mvc project with individual user accounts authentication

---
+++ PressDistributionSystemWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PressDistributionSystemWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
