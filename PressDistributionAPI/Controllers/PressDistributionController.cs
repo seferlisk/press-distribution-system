bd7dbafa062b26960075ebaf480720689c65167b
seferlisk <seferlisk@gmail.com>
2025-03-05 17:12:28 +0200

    added new WebApi project

--- PressDistributionAPI/Controllers/PressDistributionController.cs
+++ PressDistributionAPI/Controllers/PressDistributionController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace PressDistributionSystemWebApp.Controllers
+namespace PressDistributionAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
